ab3269d95d7e1daf3294b65c4d8a723014d42b20
Jason.A.Wilkins <Jason.A.Wilkins@98d04750-474c-0410-a5d7-2f7c2a400aba>
2008-05-12 00:43:50 +0000

    Can now create a visitor method named visit_Type_field(Type1 arg1, Type2 arg2, etc.)  where each arg is a member of class Type Can pass a default operator to accept that is used if no visit method is found in the current operator. didn't really mean to check in both features at the same time!  however one is a "Walkabout" feature and the other is a "WalkaboutOp" feature so they are pretty seperated and do not effect the same code.

--- trunk/DynamicVisitor/DynamicVisitor/DynamicVisitor/ObjectVisitor.cs
+++ trunk/DynamicVisitor/DynamicVisitor/DynamicVisitor/ObjectVisitor.cs
@@ -10,14 +10,14 @@ using System.Text;
 using Utils;
 
 namespace DynamicVisitor {
-    public class Operator {
+    public class WalkaboutOp {
         private static Type[] no_params = { };
         private static object[] no_args = { };
 
         Object visitor;
         Type visitor_type;
 
-        public Operator(Object visitor)
+        public WalkaboutOp(Object visitor)
         {
             if (null == visitor) throw new ArgumentNullException("visitor");
 
@@ -62,7 +62,76 @@ namespace DynamicVisitor {
             }
         }
 
-        bool invokeTerminal(string method_name, Type type, object value, out Operator new_op)
+        string make_type_string(params Type[] types)
+        {
+            StringBuilder sb = new StringBuilder();
+            bool is_first = true;
+
+            foreach (Type t in types) {
+                if (is_first) {
+                    is_first = false;
+                } else {
+                    sb.Append(", ");
+                }
+
+                sb.Append(t.Name);
+            }
+
+            return sb.ToString();
+        }
+
+        bool invokeInline(string prefix, Type type, string name, object value, out WalkaboutOp new_op)
+        {
+            if (value != null) {
+                MemberInfo[] graph_props = type.FindMembers(MemberTypes.Property|MemberTypes.Field, BindingFlags.Public|BindingFlags.Instance, null, null);
+                List<Type> param_list = new List<Type>();
+                List<Object> args_list = new List<Object>();
+
+                foreach (MemberInfo p in graph_props) {
+                    if (MemberTypes.Field == p.MemberType) {
+                        FieldInfo fi = type.GetField(p.Name);
+                        args_list.Add(fi.GetValue(value));
+                        param_list.Add(fi.FieldType);
+                    } else {
+                        PropertyInfo pi = type.GetProperty(p.Name);
+                        if (0 == pi.GetIndexParameters().Length) {
+                            args_list.Add(pi.GetValue(value, null));
+                            param_list.Add(pi.PropertyType);
+                        }
+                    }
+                }
+
+                Type[] param_array = new Type[param_list.Count];
+                Object[] arg_array = new Object[args_list.Count];
+
+                param_list.CopyTo(param_array);
+                args_list.CopyTo(arg_array);
+
+                string method_name = prefix+"_"+type.Name+(name!=null?"_"+name:"");
+                MethodInfo mi = visitor_type.GetMethod(method_name, param_array);
+
+                if (mi != null) {
+                    Object new_visitor = mi.Invoke(visitor, arg_array);
+
+                    if (null == new_visitor) {
+                        new_op = null;
+                        Trace.Warning("debug: {0}.{1}({2}) did not return a new visitor", visitor_type.FullName, method_name, make_type_string(param_array));
+                    } else {
+                        new_op = new WalkaboutOp(new_visitor);
+                    }
+
+                    return true;
+                } else {
+                    Trace.Warning("debug: could not find method {0}.{1}({2}), skipping", visitor_type.FullName, method_name, make_type_string(param_array));
+                }
+            }
+
+            new_op = null;
+
+            return false;
+        }
+
+        bool invokeTerminal(string method_name, Type type, object value, out WalkaboutOp new_op)
         {
             while (type != null) {
                 Type[] types = { type };
@@ -71,12 +140,12 @@ namespace DynamicVisitor {
                 if (mi != null) {
                     object[] args = { value };
                     Object new_visitor = mi.Invoke(visitor, args);
-                    
+
                     if (null == new_visitor) {
                         new_op = null;
                         Trace.Warning("debug: {0}.{1}({2}) did not return a new visitor", visitor_type.FullName, method_name, type.FullName);
                     } else {
-                        new_op = new Operator(new_visitor);
+                        new_op = new WalkaboutOp(new_visitor);
                     }
 
                     return true;
@@ -91,7 +160,7 @@ namespace DynamicVisitor {
             return false;
         }
 
-        bool innerCall(string method_name, out Operator new_method, bool is_last)
+        bool innerCall(string method_name, out WalkaboutOp new_method, bool is_last)
         {
             MethodInfo mi = visitor_type.GetMethod(method_name, no_params);
 
@@ -102,7 +171,7 @@ namespace DynamicVisitor {
                     new_method = null;
                     Trace.Verbose("debug: {0} did not return a new visitor", method_name);
                 } else {
-                    new_method = new Operator(new_visitor);
+                    new_method = new WalkaboutOp(new_visitor);
                 }
 
                 return true;
@@ -115,7 +184,7 @@ namespace DynamicVisitor {
             }
         }
 
-        bool invokeNonTerminal(string prefix, Type type, string name, out Operator new_method)
+        bool invokeNonTerminal(string prefix, Type type, string name, out WalkaboutOp new_method)
         {
             while (type.BaseType != null) {
                 if (!type.IsGenericType) {
@@ -130,7 +199,7 @@ namespace DynamicVisitor {
             return innerCall(prefix+(name!=null?"_"+name:""), out new_method, true);
         }
 
-        public bool child(NameTypeValue node, out Operator new_op)
+        public bool child(NameTypeValue node, out WalkaboutOp new_op)
         {
             string prefix;
 
@@ -142,7 +211,8 @@ namespace DynamicVisitor {
 
             return
                 invokeNonTerminal(prefix, node.type, node.name, out new_op) ||
-                invokeTerminal(prefix+(node.name!=null?"_"+node.name:""), node.type, node.value, out new_op);
+                invokeTerminal(prefix+(node.name!=null?"_"+node.name:""), node.type, node.value, out new_op) ||
+                invokeInline(prefix, node.type, node.name, node.value, out new_op);
         }
     }
 
@@ -199,7 +269,11 @@ namespace DynamicVisitor {
                         v = fi.GetValue(graph);
                     } else {
                         PropertyInfo pi = graph_type.GetProperty(p.Name);
-                        v = pi.GetValue(graph, null);
+                        if (0 == pi.GetIndexParameters().Length) {
+                            v = pi.GetValue(graph, null);
+                        } else {
+                            continue;
+                        }
                     }
 
                     if (v != null) {
@@ -232,29 +306,49 @@ namespace DynamicVisitor {
     }
 
     class Walkabout {
-        public void accept(Object parent, Operator op)
-        {
-            if (null == parent) throw new ArgumentNullException("parent");
+        WalkaboutOp m_default_op;
 
-            accept(new NameTypeValue(null, parent.GetType(), parent), op);
+        public void accept(Object parent, WalkaboutOp op)
+        {
+            accept(parent, op, null);
         }
 
-        void accept(NameTypeValue parent, Operator op)
+        public void accept(Object parent, WalkaboutOp op, WalkaboutOp default_op)
+        {
+            if (null == parent) throw new ArgumentNullException("parent");
+            if (null == op) op = default_op;
+            if (null == op) throw new ArgumentNullException("op and default_op cannot both be null");
+
+            m_default_op = default_op;
+
+            new_accept(new NameTypeValue(null, parent.GetType(), parent), op);
+        }
+
+        void accept_each(NameTypeValue parent, WalkaboutOp op)
+        {
+            foreach (NameTypeValue child in new NameTypeValueList(parent)) {
+                WalkaboutOp new_op;
+
+                if (!op.child(child, out new_op) || (m_default_op != null && !m_default_op.child(child, out new_op))) {
+                    accept_inline(child, op);
+                } else if (new_op != null) {
+                    new_accept(child, new_op);
+                }
+            }
+        }
+
+        void accept_inline(NameTypeValue parent, WalkaboutOp op)
         {
             if (!op.parent(parent)) {
+                accept_each(parent, op);
+            }
+        }
 
+        void new_accept(NameTypeValue parent, WalkaboutOp op)
+        {
+            if (!op.parent(parent)) {
                 op.begin();
-
-                foreach (NameTypeValue child in new NameTypeValueList(parent)) {
-                    Operator new_op;
-
-                    if (!op.child(child, out new_op)) {
-                        accept(child, op);
-                    } else if (new_op != null) {
-                        accept(child, new_op);
-                    }
-                }
-
+                accept_each(parent, op);
                 op.end();
             }
         }
